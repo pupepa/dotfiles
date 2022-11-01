d01d314528e51a5a354a4de4242d93dd836236b2
pupepa <120288786+pupepa@users.noreply.github.com>
2022-11-01 13:34:10 +0900

    vim: Fix null-ls configs

--- configs/nvim/lua/rc/pluginconfig/null-ls.lua
+++ configs/nvim/lua/rc/pluginconfig/null-ls.lua
@@ -4,12 +4,20 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 
 local sources = {
   null_ls.builtins.formatting.stylua,
-  null_ls.builtins.code_actions.eslint_d,
-  null_ls.builtins.diagnostics.eslint_d,
+  null_ls.builtins.code_actions.eslint.with({
+    prefer_local = "node_modules/.bin",
+  }),
+  null_ls.builtins.diagnostics.eslint.with({
+    prefer_local = "node_modules/.bin",
+  }),
+  null_ls.builtins.formatting.eslint.with({
+    prefer_local = "node_modules/.bin",
+  }),
   null_ls.builtins.formatting.prettier.with({
     condition = function()
       return vim.fn.executable("prettier") > 0
     end,
+    prefer_local = "node_modules/.bin",
   }),
   null_ls.builtins.diagnostics.textlint.with({ filetypes = { "markdown" } }),
 }
@@ -23,7 +31,7 @@ null_ls.setup({
         group = augroup_format,
         buffer = 0,
         callback = function()
-          vim.lsp.buf.formatting_seq_sync()
+          vim.lsp.buf.format({ timeout_ms = 5000 })
         end,
       })
     end
