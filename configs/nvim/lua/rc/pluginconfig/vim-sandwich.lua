680aa09e6c2cfd8ebea9c0b578bcb47a08407b11
pupepa <120288786+pupepa@users.noreply.github.com>
2023-01-20 10:34:56 +0900

    vim: Modify settings for vim-sandwich

--- configs/nvim/lua/rc/pluginconfig/vim-sandwich.lua
+++ configs/nvim/lua/rc/pluginconfig/vim-sandwich.lua
@@ -1,21 +1,26 @@
-vim.g["sandwich#recipes"] = vim.tbl_extend('force', vim.deepcopy(vim.g['sandwich#default_recipes']), {
+vim.g["sandwich#recipes"] = vim.tbl_extend("force", vim.deepcopy(vim.g["sandwich#default_recipes"]), {
   {
-    buns = { '${', '}' },
-    input = { '$' },
-    filetype =  { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+    buns = { "${", "}" },
+    input = { "$" },
+    filetype = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
   {
-    buns = {'#{', '}' },
-    input = { '#' },
-    filetype = { 'ruby', 'eruby' }
+    buns = { "#{", "}" },
+    input = { "#" },
+    filetype = { "ruby", "eruby" },
   },
   {
-    buns = { '「', '」' },
-    input = { 'c' },
+    buns = { "「", "」" },
+    input = { "c" },
   },
   {
-    buns = { '[[', ']]' },
-    input = { 'l' },
-    filetype = { 'markdown' },
+    buns = { "[[", "]]" },
+    input = { "l" },
+    filetype = { "markdown" },
+  },
+  {
+    buns = { "[", "]()" },
+    input = { "L" },
+    filetype = { "markdown" },
   },
 })
