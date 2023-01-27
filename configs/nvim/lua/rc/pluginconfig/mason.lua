342103f7a07168c4d5f1caae37b9ca553a2fed53
pupepa <120288786+pupepa@users.noreply.github.com>
2023-01-27 13:19:03 +0900

    vim: Define LSP to force installation

--- configs/nvim/lua/rc/pluginconfig/mason.lua
+++ configs/nvim/lua/rc/pluginconfig/mason.lua
@@ -1,7 +1,20 @@
 require("mason").setup()
 
 local mason_lspconfig = require("mason-lspconfig")
-mason_lspconfig.setup()
+mason_lspconfig.setup({
+  ensure_installed = {
+    "bashls",
+    "cssls",
+    "dockerls",
+    "html",
+    "jsonls",
+    "solargraph",
+    "sumneko_lua",
+    "tailwindcss",
+    "tsserver",
+    "vimls"
+  }
+})
 mason_lspconfig.setup_handlers({
   function(server_name)
     local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
