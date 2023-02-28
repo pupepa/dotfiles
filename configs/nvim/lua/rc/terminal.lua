e6beba9e68ca1efcbff7cf59f86cf85b8895f74f
pupepa <120288786+pupepa@users.noreply.github.com>
2023-02-28 15:47:05 +0900

    vim: Change keymap for Terminal mode

--- configs/nvim/lua/rc/terminal.lua
+++ configs/nvim/lua/rc/terminal.lua
@@ -2,5 +2,5 @@ if vim.fn.has("nvim") == 1 and vim.fn.executable("nvr") == 1 then
   vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
 end
 
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n><Plug>(esc)")
-vim.keymap.set("n", "<Plug>(esc)<Esc>", "i<Esc>")
+vim.keymap.set("t", "jk", "<C-\\><C-n><Plug>(j)")
+vim.keymap.set("n", "<Plug>(j)j", "ijk")
