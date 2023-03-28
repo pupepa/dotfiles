6950b0db681fbf2fdacabf2edef52de866fa7fd5
pupepa <120288786+pupepa@users.noreply.github.com>
2023-03-29 07:35:16 +0900

    Fix for changing account name

--- scripts/common.bash
+++ scripts/common.bash
@@ -4,7 +4,7 @@ set -x
 
 export CURRENT_DIR DOTFILES_DIR
 CURRENT_DIR="$(cd "$(dirname "$0")" || exit 1; pwd)"
-DOTFILES_DIR="${DOTFILES_DIR:-$HOME/ghq/github.com/dre-jp/dotfiles}"
+DOTFILES_DIR="${DOTFILES_DIR:-$HOME/ghq/github.com/pupepa/dotfiles}"
 
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
