236d9dcef7403aca8cf6d99b08ca06e9b720167e
pupepa <120288786+pupepa@users.noreply.github.com>
2023-06-30 07:35:35 +0900

    vim: Change keymap to change normal terminal mode

--- configs/nvim/lua/rc/terminal.lua
+++ configs/nvim/lua/rc/terminal.lua
@@ -2,5 +2,5 @@ if vim.fn.has("nvim") == 1 and vim.fn.executable("nvr") == 1 then
   vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
 end
 
-vim.keymap.set("t", "jk", "<C-\\><C-n><Plug>(j)")
+vim.keymap.set("t", "zz", "<C-\\><C-n><Plug>(j)")
 vim.keymap.set("n", "<Plug>(j)j", "ijk")
