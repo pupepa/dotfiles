bbeef4a8f219b5c53f264a7b0ac617934b4ccf6d
pupepa <120288786+pupepa@users.noreply.github.com>
2023-09-20 08:18:27 +0900

    vim: Add rainbow-delimiters.nvim

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -477,6 +477,17 @@ return require("lazy").setup({
     end,
   },
 
+  -- Rainbow delimiters for Neovim with Tree-sitter
+  -- https://github.com/hiphish/rainbow-delimiters.nvim
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "BufReadPost",
+    config = function()
+      -- patch https://github.com/nvim-treesitter/nvim-treesitter/issues/1124
+      vim.cmd.edit({ bang = true })
+    end,
+  },
+
   -----------------------------------------------------------------------------------------
   -- textobj
   -----------------------------------------------------------------------------------------
