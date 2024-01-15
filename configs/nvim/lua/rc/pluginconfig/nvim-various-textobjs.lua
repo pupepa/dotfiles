f0cd2bc1e91d5fbb1683f0e37b958857194d133c
pupepa <120288786+pupepa@users.noreply.github.com>
2024-01-15 09:17:23 +0900

    vim: Add keymap for nvim-various-textobjs

--- configs/nvim/lua/rc/pluginconfig/nvim-various-textobjs.lua
+++ configs/nvim/lua/rc/pluginconfig/nvim-various-textobjs.lua
@@ -1,3 +1,5 @@
 require("various-textobjs").setup({ useDefaultKeymaps = true })
 
 vim.keymap.set({ "o", "x" }, "ae", "<Cmd>lua require('various-textobjs').entireBuffer()<CR>")
+vim.keymap.set({ "o", "x" }, "ib", "<Cmd>lua require('various-textobjs').anyQuote('inner')<CR>")
+vim.keymap.set({ "o", "x" }, "ab", "<Cmd>lua require('various-textobjs').anyQuote('outer')<CR>")
