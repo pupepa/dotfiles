61ef406b458f5f4e27100ec7a5922820ab1bb133
pupepa <120288786+pupepa@users.noreply.github.com>
2024-08-26 13:41:40 +0900

    vim: Add git-blame.nvim plugin

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -541,6 +541,19 @@ return require("lazy").setup({
     end,
   },
 
+  -- Git Blame plugin for Neovim written in Lua
+  -- https://github.com/f-person/git-blame.nvim
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+      enabled = false, -- if you want to enable the plugin
+      message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+      date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
+
   -----------------------------------------------------------------------------------------
   -- textobj
   -----------------------------------------------------------------------------------------
