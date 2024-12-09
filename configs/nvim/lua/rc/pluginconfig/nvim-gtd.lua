b9c3dd4dc07adfe73beb2e44fd470bd96671496b
pupepa <120288786+pupepa@users.noreply.github.com>
2024-12-09 16:20:56 +0900

    vim: Modify the settings of nvim-gtd

--- configs/nvim/lua/rc/pluginconfig/nvim-gtd.lua
+++ configs/nvim/lua/rc/pluginconfig/nvim-gtd.lua
@@ -24,39 +24,6 @@ require("gtd").setup({
       },
     },
   },
-  get_buffer_path = function()
-    return vim.fn["vimrc#get_buffer_path"]()
-  end,
-  on_location = function(context, location)
-    local row = location.targetSelectionRange.start.line + 1
-    local col = location.targetSelectionRange.start.character + 1
-    if row == 1 and col == 1 then
-      row = nil
-      col = nil
-    end
-    vim.fn["vimrc#open"](context.command, {
-      filename = vim.uri_to_fname(location.targetUri),
-      lnum = row,
-      col = col,
-    }, vim.fn.winnr())
-  end,
-  on_locations = function(context, locations)
-    local items = {}
-    for i, location in ipairs(locations) do
-      table.insert(items, {
-        id = i,
-        title = vim.uri_to_fname(location.targetUri),
-        filename = vim.uri_to_fname(location.targetUri),
-        lnum = location.targetSelectionRange.start.line + 1,
-        col = location.targetSelectionRange.start.character + 1,
-      })
-    end
-    vim.fn["candle#start"]({ item = items }, {
-      action = {
-        default = "edit",
-      },
-    })
-  end,
 })
 
 vim.keymap.set("n", "gf<CR>", function()
