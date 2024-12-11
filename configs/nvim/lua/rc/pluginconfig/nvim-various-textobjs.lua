4d6b2a4983f6f2a5519455431f9876e0cc9abbed
pupepa <120288786+pupepa@users.noreply.github.com>
2024-12-12 08:31:48 +0900

    vim: Add url text object

--- configs/nvim/lua/rc/pluginconfig/nvim-various-textobjs.lua
+++ configs/nvim/lua/rc/pluginconfig/nvim-various-textobjs.lua
@@ -1,5 +1,6 @@
-require("various-textobjs").setup({ useDefaultKeymaps = false })
+require("various-textobjs").setup({ useDefaults = false })
 
 vim.keymap.set({ "o", "x" }, "ae", "<Cmd>lua require('various-textobjs').entireBuffer()<CR>")
 vim.keymap.set({ "o", "x" }, "ib", "<Cmd>lua require('various-textobjs').anyQuote('inner')<CR>")
 vim.keymap.set({ "o", "x" }, "ab", "<Cmd>lua require('various-textobjs').anyQuote('outer')<CR>")
+vim.keymap.set({ "o", "x" }, "iu", '<cmd>lua require("various-textobjs").url()<CR>')
