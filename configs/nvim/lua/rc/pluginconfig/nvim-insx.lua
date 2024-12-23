f7908320a7197944cda8c7655f782e362ddeff20
pupepa <120288786+pupepa@users.noreply.github.com>
2024-12-23 09:08:34 +0900

    vim: Remove auto_tag function for insx

--- configs/nvim/lua/rc/pluginconfig/nvim-insx.lua
+++ configs/nvim/lua/rc/pluginconfig/nvim-insx.lua
@@ -4,37 +4,8 @@ local fast_wrap = require("insx.recipe.fast_wrap")
 local fast_break = require("insx.recipe.fast_break")
 local helper = require("insx.helper")
 
-local function auto_tag()
-  local search = {}
-  search.Tag = {
-    Open = [=[<\(\w\+\)\%(\s\+.\{-}\)\?]=],
-    Close = [=[</\w\+>]=],
-  }
-  return {
-    builtin = {
-      ["html"] = {
-        {
-          action = function(ctx)
-            local name = ctx.before():match("<(%w+)")
-            local row, col = ctx.row(), ctx.col()
-            ctx.move(row, col + 1)
-            ctx.send(([[</%s>]]):format(name))
-            ctx.move(row, col + 1)
-          end,
-          enabled = function(ctx)
-            return helper.regex.match(ctx.before(), search.Tag.Open) ~= nil
-                and helper.regex.match(ctx.after(), helper.search.Tag.Close) == nil
-          end,
-        },
-      },
-    },
-  }
-end
-
--- endwise
 local endwise = require("insx.recipe.endwise")
 insx.add("<CR>", endwise(endwise.builtin))
-insx.add(">", endwise(auto_tag().builtin))
 
 -- quotes
 for _, quote in ipairs({ '"', "'" }) do
