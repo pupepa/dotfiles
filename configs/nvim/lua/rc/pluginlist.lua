550ec6e0dba3127083579f76036bd63dddc9c9f9
pupepa <120288786+pupepa@users.noreply.github.com>
2025-08-08 13:19:48 +0900

    vim: Fix failure to update rainbow-delimiters.nvim

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -496,6 +496,7 @@ return require("lazy").setup({
   {
     "HiPhish/rainbow-delimiters.nvim",
     event = "BufReadPost",
+    submodules = false,
     config = function()
       -- patch https://github.com/nvim-treesitter/nvim-treesitter/issues/1124
       vim.cmd.edit({ bang = true })
