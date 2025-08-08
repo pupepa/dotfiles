82ca57a573ead4e90b8e14cc48c832464f0baec6
pupepa <120288786+pupepa@users.noreply.github.com>
2025-08-08 13:48:39 +0900

    vim: Fix settings for better-escape

--- configs/nvim/lua/rc/pluginconfig/better-escape.lua
+++ configs/nvim/lua/rc/pluginconfig/better-escape.lua
@@ -1,1 +1,17 @@
-require("better_escape").setup()
+require("better_escape").setup({
+  timeout: vim.o.timeoutlen,
+  default_mappings: false,
+  mappings: {
+    i: {
+      j: {
+        j: "<Esc>",
+        k: "<Esc>",
+      },
+    },
+    t: {
+      j: {
+        k: "<C-\\><C-n>",
+      },
+    },
+  },
+});
