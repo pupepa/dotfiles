e63a34ab35f3333a71e189c6729840f89c335724
pupepa <120288786+pupepa@users.noreply.github.com>
2025-12-19 13:50:49 +0900

    vim: Add kulala.nvim HTTP client plugin

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -1308,4 +1308,37 @@ return require("lazy").setup({
       },
     },
   },
+
+  --A fully-featured 🤏 HTTP-client 🐼 interface 🖥️ for Neovim ❤️.
+  -- https://github.com/mistweaverco/kulala.nvim
+  {
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>Rs", desc = "Send request" },
+      { "<leader>Ra", desc = "Send all requests" },
+      { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    opts = {
+      kulala_keymaps_prefix = "",
+      global_keymaps = {
+        ["Send request"] = {
+          "<leader>Rs",
+          function()
+            require("kulala").run()
+          end,
+          mode = { "n", "v" },
+          desc = "Send request",
+        },
+        ["Send all requests"] = {
+          "<leader>Ra",
+          function()
+            require("kulala").run_all()
+          end,
+          mode = { "n", "v" },
+          ft = { "http", "rest" },
+        },
+      },
+    },
+  },
 })
