125814533ebcdb80557204aa47b5f3acabc14bae
pupepa <120288786+pupepa@users.noreply.github.com>
2025-12-30 09:24:06 +0900

    vim: Modify configuration for trouble.nvim

--- configs/nvim/lua/rc/pluginconfig/trouble.lua
+++
@@ -1,8 +0,0 @@
-require("trouble").setup({})
-
-vim.keymap.set("n", "<Leader>xx", "<cmd>TroubleToggle<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>xw", "<cmd>Trouble workspace_diagnostics<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>xd", "<cmd>Trouble document_diagnostics<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>xl", "<cmd>Trouble loclist<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>xq", "<cmd>Trouble quickfix<CR>", { silent = true })
-vim.keymap.set("n", "gR", "<Cmd>Trouble lsp_references<CR>", { silent = true })
