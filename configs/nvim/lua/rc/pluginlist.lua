03d5bb3a64f1a6fe19114eac347cc07f927d6575
pupepa <120288786+pupepa@users.noreply.github.com>
2026-01-28 11:17:04 +0900

    vim: Use smart-splits.nvim to manage window instead of tmux.nvim

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -2017,25 +2017,48 @@ return require("lazy").setup({
   {
     "mrjones2014/smart-splits.nvim",
     keys = {
+      -- focus windows
+      {
+        "<C-h>",
+        "<cmd>lua require('smart-splits').move_cursor_left()<cr>",
+        desc = "Focus Window Left",
+      },
+      {
+        "<C-l>",
+        "<cmd>lua require('smart-splits').move_cursor_right()<cr>",
+        desc = "Focus Window Right",
+      },
+      {
+        "<C-j>",
+        "<cmd>lua require('smart-splits').move_cursor_down()<cr>",
+        desc = "Focus Window Down",
+      },
+      {
+        "<C-k>",
+        "<cmd>lua require('smart-splits').move_cursor_up()<cr>",
+        desc = "Focus Window Up",
+      },
+
+      -- resize windows
       {
         "<S-Left>",
-        ":lua require('smart-splits').resize_left()<CR>",
-        silent = true,
-      },
-      {
-        "<S-Down>",
-        ":lua require('smart-splits').resize_down()<CR>",
-        silent = true,
-      },
-      {
-        "<S-Up>",
-        ":lua require('smart-splits').resize_up()<CR>",
-        silent = true,
+        "<cmd>lua require('smart-splits').resize_left()<cr>",
+        desc = "Resize Window Left",
       },
       {
         "<S-Right>",
-        ":lua require('smart-splits').resize_right()<CR>",
-        silent = true,
+        "<cmd>lua require('smart-splits').resize_right()<cr>",
+        desc = "Resize Window Right",
+      },
+      {
+        "<S-Down>",
+        "<cmd>lua require('smart-splits').resize_down()<cr>",
+        desc = "Resize Window Down",
+      },
+      {
+        "<S-Up>",
+        "<cmd>lua require('smart-splits').resize_up()<cr>",
+        desc = "Resize Window Up",
       },
     },
     lazy = true,
@@ -2071,22 +2094,6 @@ return require("lazy").setup({
       "tyru/open-browser.vim",
     },
     cmd = { "OpenGithubFile", "OpenGithubIssue", "OpenGithubPullReq", "OpenGithubProject" },
-  },
-
-  -- tmux integration for nvim features pane movement and resizing from within nvim.
-  -- https://github.com/aserowy/tmux.nvim
-  {
-    "aserowy/tmux.nvim",
-    keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
-    lazy = true,
-    opts = {
-      {
-        copy_sync = {
-          enable = false,
-          sync_registers = false,
-        },
-      },
-    },
   },
 
   -- A vim plugin to perform diffs on blocks of code
