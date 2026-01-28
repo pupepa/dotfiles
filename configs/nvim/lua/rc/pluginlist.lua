a0e2d24adeddbeca80a850e5e10ae1a3960acc1c
pupepa <120288786+pupepa@users.noreply.github.com>
2026-01-28 11:18:00 +0900

    vim: Add hlargs.nvim plugin

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -679,6 +679,16 @@ return require("lazy").setup({
     lazy = true,
   },
 
+  -- Highlight arguments' definitions and usages, using Treesitter
+  -- https://github.com/m-demare/hlargs.nvim
+  {
+    "m-demare/hlargs.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("hlargs").setup()
+    end,
+  },
+
   -----------------------------------------------------------------------------------------
   -- Fuzzy Finder
   -----------------------------------------------------------------------------------------
