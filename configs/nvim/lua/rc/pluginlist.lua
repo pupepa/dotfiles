ad46c7d7e2650d84211f5caaec6d911e3d1c16f2
pupepa <120288786+pupepa@users.noreply.github.com>
2026-02-24 07:49:43 +0900

    vim: Modify settings for Previm

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -2278,9 +2278,16 @@ return require("lazy").setup({
   {
     "previm/previm",
     ft = { "markdown", "asciidoc" },
+    cmd = { "PrevimOpen" },
     dependencies = {
       "tyru/open-browser.vim",
     },
+    config = function()
+      vim.g.previm_enable_realtime = 1
+      vim.g.previm_code_language_show = 1
+      vim.g.previm_disable_default_css = 1
+      vim.g.previm_custom_css_path = vim.fn.stdpath("config") .. "/css/github-markdown.css"
+    end,
   },
 
   -- 📑 Delete multiple vim buffers based on different conditions
