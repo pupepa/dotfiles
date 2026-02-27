eec64310ae645d179761c997292d6626abb5e917
pupepa <120288786+pupepa@users.noreply.github.com>
2026-02-27 14:17:58 +0900

    zsh: Settings for git-wt

--- configs/zsh/git.zsh
+++ configs/zsh/git.zsh
@@ -1,1 +1,9 @@
 export PATH=/usr/local/share/git-core/contrib/diff-highlight:$PATH$
+
+# git-wt
+eval "$(git wt --init zsh)"
+
+wcd() {
+  cd $(git-wt | fzf --header-lines=1 | awk '{if ($1 == "*") print $2; else print $1}')
+}
+
