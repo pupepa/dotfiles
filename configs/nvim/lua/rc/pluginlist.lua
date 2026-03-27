46067f1e38f661b8c0382f86c465e31a4cb84a26
pupepa <120288786+pupepa@users.noreply.github.com>
2026-03-27 13:46:22 +0900

    vim: Add render-markdown.nvim

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -1761,6 +1761,31 @@ return require("lazy").setup({
     },
   },
 
+  -- Plugin to improve viewing Markdown files in Neovim
+  -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      heading = {
+        icons = {},
+        position = "inline",
+        width = "block",
+      },
+      code = {
+        conceal_delimiters = false,
+      },
+      pipe_table = { style = "normal" },
+      win_options = {
+        conceallevel = {
+          rendered = 0,
+        },
+      },
+    },
+  },
+
   -----------------------------------------------------------------------------------------
   -- Moving Cursor
   -----------------------------------------------------------------------------------------
