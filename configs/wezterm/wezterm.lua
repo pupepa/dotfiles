61517c53ad61a7c39e00b3a01f639bc8c8b9dbb2
pupepa <120288786+pupepa@users.noreply.github.com>
2026-04-08 15:46:33 +0900

    chore(wezterm): add macos_forward_to_ime_modifier_mask for macSKK

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
+
 config.font = wezterm.font("UDEV Gothic NF", { weight = "Bold" })
 config.font_size = 14.9
 config.color_scheme = "tokyonight"
