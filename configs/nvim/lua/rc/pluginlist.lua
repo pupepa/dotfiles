fa2039d8bd5fd380527d9326de2bf948b9dc9c38
pupepa <120288786+pupepa@users.noreply.github.com>
2026-04-17 13:50:04 +0900

    chore(nvim): add filetype constraint to render-markdown.nvim

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -1795,6 +1795,7 @@ return require("lazy").setup({
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ft = { "markdown" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
