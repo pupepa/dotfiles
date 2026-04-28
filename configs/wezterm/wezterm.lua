9476fc1bd85910733f76783011a7b0620a0fc0e2
pupepa <120288786+pupepa@users.noreply.github.com>
2026-04-28 16:07:49 +0900

    chore(wezterm): configure font fallbacks and bold font rules

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -3,8 +3,17 @@ local config = wezterm.config_builder()
 
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 
-config.font = wezterm.font("UDEV Gothic NF", { weight = "Bold" })
-config.font_size = 14.9
+config.font = wezterm.font_with_fallback({ "UDEV Gothic NF" })
+config.font_rules = {
+  {
+    intensity = "Bold",
+    font = wezterm.font_with_fallback({
+      { family = "UDEV Gothic NF", weight = "Bold" },
+    }),
+  },
+}
+config.warn_about_missing_glyphs = false
+config.font_size = 14
 config.color_scheme = "tokyonight"
 config.use_ime = true
 config.hide_tab_bar_if_only_one_tab = true
