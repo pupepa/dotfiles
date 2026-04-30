acefbfd7e151f968d1f98a63d2054ecd6ffe6de3
pupepa <120288786+pupepa@users.noreply.github.com>
2026-04-30 14:13:04 +0900

    feat(nvim): add nvim-scrollbar plugin

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -1358,6 +1358,26 @@ return require("lazy").setup({
     cmd = "CodeDiff",
   },
 
+  -- Extensible Neovim Scrollbar
+  -- https://github.com/petertriho/nvim-scrollbar
+  {
+    "petertriho/nvim-scrollbar",
+    event = { "BufNewFile", "BufReadPre" },
+    config = function()
+      local colors = require("tokyonight.colors").setup()
+      require("scrollbar").setup({
+        handle = {
+          color = colors.fg,
+        },
+      })
+    end,
+    dependencies = {
+      "kevinhwang91/nvim-hlslens",
+      "lewis6991/gitsigns.nvim",
+      "folke/tokyonight.nvim",
+    },
+  },
+
   -----------------------------------------------------------------------------------------
   -- textobj
   -----------------------------------------------------------------------------------------
