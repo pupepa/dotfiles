66eaa24491cb234a30db118486b384783f8b1fdf
pupepa <120288786+pupepa@users.noreply.github.com>
2026-04-30 14:16:51 +0900

    feat(wezterm): add hyperlink rules for URLs and email addresses

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -249,6 +249,53 @@ smart_splits.apply_to_config(config, {
   log_level = "info",
 })
 
+config.hyperlink_rules = {
+  -- Matches: a URL in parens: (URL)
+  {
+    regex = "\\((\\w+://\\S+)\\)",
+    format = "$1",
+    highlight = 1,
+  },
+  -- Matches: a URL in brackets: [URL]
+  {
+    regex = "\\[(\\w+://\\S+)\\]",
+    format = "$1",
+    highlight = 1,
+  },
+  -- Matches: a URL in curly braces: {URL}
+  {
+    regex = "\\{(\\w+://\\S+)\\}",
+    format = "$1",
+    highlight = 1,
+  },
+  -- Matches: a URL in angle brackets: <URL>
+  {
+    regex = "<(\\w+://\\S+)>",
+    format = "$1",
+    highlight = 1,
+  },
+  -- Then handle URLs not wrapped in brackets
+  {
+    -- Before
+    --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+    --format = '$0',
+    -- After
+    regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+    format = "$1",
+    highlight = 1,
+  },
+  -- implicit mailto link
+  {
+    regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+    format = "mailto:$0",
+  },
+}
+
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = "https://github.com/$1/$3",
+})
+
 require("edit_scrollback").apply_to_config(config)
 
 return config
