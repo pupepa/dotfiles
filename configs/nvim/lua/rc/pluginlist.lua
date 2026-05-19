5c612609741d963e72effb91a2630022dfc36fd9
pupepa <120288786+pupepa@users.noreply.github.com>
2026-05-19 15:40:19 +0900

    feat(nvim): add cellwidths.nvim plugin for font width configuration

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -1378,6 +1378,25 @@ return require("lazy").setup({
     },
   },
 
+  -- https://github.com/delphinus/cellwidths.nvim
+  {
+    "delphinus/cellwidths.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("cellwidths").setup({
+        name = "cica"
+        -- name = "user/custom",
+        -- ---@param cw cellwidths
+        -- fallback = function(cw)
+        --   cw.add {
+        --     { 0x2160, 0x2169, 2 },
+        --     { 0x2460, 0x2469, 2 }
+        --   }
+        -- end,
+      })
+    end,
+  },
+
   -----------------------------------------------------------------------------------------
   -- textobj
   -----------------------------------------------------------------------------------------
