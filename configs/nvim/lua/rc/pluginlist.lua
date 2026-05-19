bcec4797153e9d35c07fe9a9d01c5f23bcb77e96
pupepa <120288786+pupepa@users.noreply.github.com>
2026-05-19 15:45:35 +0900

    feat(nvim): add git.nvim plugin

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -2684,6 +2684,30 @@ return require("lazy").setup({
       "nvim-tree/nvim-web-devicons",
     },
   },
+
+  -- git.nvim is the simple clone of the plugin vim-fugitive which is written in Lua.
+  -- https://github.com/dinhhuy258/git.nvim
+  {
+    "dinhhuy258/git.nvim",
+    cmd = "Git",
+    keys = {
+      {
+        "<leader>go",
+        function()
+          local status, git = pcall(require, "git")
+          if status then
+            git.setup({
+              default_mappings = false,
+              keymaps = {
+                browse = "<Leader>go",
+              },
+            })
+          end
+        end,
+        desc = "Browse File",
+      },
+    },
+  },
 }, {
   rocks = {
     enabled = false,
