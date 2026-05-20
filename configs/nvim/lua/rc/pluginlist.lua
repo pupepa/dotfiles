dabfe5137ef3bfd20067694599dbb84aa8b177e7
pupepa <120288786+pupepa@users.noreply.github.com>
2026-05-20 15:44:17 +0900

    chore(nvim): update lua lsp library references in pluginlist

--- configs/nvim/lua/rc/pluginlist.lua
+++ configs/nvim/lua/rc/pluginlist.lua
@@ -447,10 +447,11 @@ return require("lazy").setup({
     ft = "lua", -- only load on lua files
     opts = {
       library = {
-        {
-          path = "luvit-meta/library",
-          words = { "vim%.uv" },
-        },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "LazyVim", words = { "LazyVim" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
