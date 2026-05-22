9956918f5add753bea6021dfb4124426c10d7c52
pupepa <120288786+pupepa@users.noreply.github.com>
2026-05-22 14:43:42 +0900

    feat(which-key): add buffer keymaps keybinding

--- configs/nvim/lua/rc/plugins/which-key.lua
+++ configs/nvim/lua/rc/plugins/which-key.lua
@@ -9,9 +9,15 @@ return {
       vim.o.timeoutlen = 300
     end,
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     },
-  },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Keymaps (which-key)",
+      },
+    }
+  }
 }
