0f340b87b7b8364b8c955fc4497779ac9063b63f
pupepa <120288786+pupepa@users.noreply.github.com>
2026-07-06 14:42:15 +0900

    chore(nvim): remove loaded_shada_plugin

--- configs/nvim/lua/rc/base.lua
+++ configs/nvim/lua/rc/base.lua
@@ -12,7 +12,6 @@ vim.g.loaded_man = 1
 -- vim.g.loaded_matchparen = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_remote_plugins = 1
-vim.g.loaded_shada_plugin = 1
 vim.g.loaded_spellfile_plugin = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
