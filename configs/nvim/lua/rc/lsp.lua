31ea0374b01d6d8d40e305124aaae23bf42a08c5
pupepa <120288786+pupepa@users.noreply.github.com>
2026-08-07 09:27:59 +0900

    feat(lsp): enhance dockerfile-language-server with docker-compose support

--- configs/nvim/lua/rc/lsp.lua
+++ configs/nvim/lua/rc/lsp.lua
@@ -47,8 +47,26 @@ vim.lsp.config("bash-language-server", {
 })
 
 vim.lsp.config("dockerfile-language-server", {
-  cmd = { "docker-langserver", "--stdio" },
-  filetypes = { "dockerfile" },
+  cmd = { "docker-language-server", "start", "--stdio" },
+  filetypes = { "dockerfile", "yaml.docker-compose" },
+  get_language_id = function(_, ftype)
+    if ftype == "yaml.docker-compose" or ftype:lower():find("ya?ml") then
+      return "dockercompose"
+    else
+      return ftype
+    end
+  end,
+  root_markers = {
+    "Dockerfile",
+    "docker-compose.yaml",
+    "docker-compose.yml",
+    "compose.yaml",
+    "compose.yml",
+    "docker-bake.json",
+    "docker-bake.hcl",
+    "docker-bake.override.json",
+    "docker-bake.override.hcl",
+  },
 })
 
 vim.lsp.config("ruby_lsp", {
