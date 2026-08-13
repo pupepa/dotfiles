8e502bc7dc299201c63e862c711ddec1f2f9a0c9
pupepa <120288786+pupepa@users.noreply.github.com>
2026-08-14 07:39:21 +0900

    fix(zsh): guard git-wt init when unavailable

--- configs/zsh/git.zsh
+++ configs/zsh/git.zsh
@@ -1,7 +1,9 @@
 export PATH="$(brew --prefix git)/share/git-core/contrib/diff-highlight:$PATH"
 
 # git-wt
-eval "$(git wt --init zsh)"
+if (( $+commands[git-wt] )); then
+  eval "$(git wt --init zsh)"
+fi
 
 wcd() {
   cd $(git-wt | fzf --header-lines=1 | awk '{if ($1 == "*") print $2; else print $1}')
