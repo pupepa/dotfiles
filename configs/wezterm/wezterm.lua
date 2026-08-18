30600cdf21cf2460bccba3633075b9e9e343e719
pupepa <120288786+pupepa@users.noreply.github.com>
2026-08-18 15:00:57 +0900

    chore(herdr): use herdr

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -69,7 +69,7 @@ wezterm.on("trigger-nvim-with-scrollback", function(window, pane)
   os.remove(name)
 end)
 
-config.leader = { key = "t", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = ";", mods = "CTRL", timeout_milliseconds = 2000 }
 
 config.keys = {
   {
@@ -196,6 +196,7 @@ config.keys = {
 
   { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode }, -- enter copy mode
   { key = "]", mods = "LEADER", action = wezterm.action.PasteFrom("Clipboard") },
+  { key = "t", mods = "CTRL", action = wezterm.action.SendString("\x14") },
 }
 
 config.key_tables = {
@@ -235,30 +236,6 @@ wezterm.on("update-right-status", function(window, pane)
   end
   window:set_right_status(name or "")
 end)
-
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
--- you can put the rest of your Wezterm config here
-smart_splits.apply_to_config(config, {
-  -- the default config is here, if you'd like to use the default keys,
-  -- you can omit this configuration table parameter and just use
-  -- smart_splits.apply_to_config(config)
-
-  -- directional keys to use in order of: left, down, up, right
-  direction_keys = { "h", "j", "k", "l" },
-  -- if you want to use separate direction keys for move vs. resize, you
-  -- can also do this:
-  -- direction_keys = {
-  --   move = { 'h', 'j', 'k', 'l' },
-  --   resize = { 'LeftArrow', 'DownArrow', 'UpArrow', 'RightArrow' },
-  -- },
-  -- modifier keys to combine with direction_keys
-  modifiers = {
-    move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
-    resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
-  },
-  -- log level to use: info, warn, error
-  log_level = "info",
-})
 
 config.hyperlink_rules = {
   -- Matches: a URL in parens: (URL)
