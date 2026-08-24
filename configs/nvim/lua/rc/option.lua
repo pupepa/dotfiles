f8f38d5cafeeb02c4a14ab364124215d414e6f7b
pupepa <120288786+pupepa@users.noreply.github.com>
2026-08-24 11:57:10 +0900

    chore(nvim): remove redundant option configurations

--- configs/nvim/lua/rc/option.lua
+++ configs/nvim/lua/rc/option.lua
@@ -13,7 +13,6 @@ vim.opt.ttimeoutlen = 10 -- Normal モードへの切り替えの遅延をなく
 vim.opt.tabstop = 2 -- タブサイズ
 vim.opt.shiftwidth = 2 -- シフト移動幅
 vim.opt.expandtab = true -- タブの代わりに空白文字を挿入する
-vim.opt.smarttab = true -- 行頭の余白内で Tab を打ち込むと、'shiftwidth' の数だけインデントする。
 
 -- Clipboard
 if vim.fn.has("clipboard") == 1 then
@@ -21,12 +20,10 @@ if vim.fn.has("clipboard") == 1 then
 end
 
 -- Insert
-vim.opt.backspace = "indent,eol,start" -- バックスペースでインデントや改行を削除できるようにする
 vim.opt.formatoptions:append("m")
 vim.opt.formatoptions:append("M")
 
 -- Command
-vim.opt.wildmenu = true -- コマンドライン補完を拡張
 vim.opt.wildignorecase = true -- 補完の時に大文字小文字を無視する
 vim.opt.wildignore = "*.pyc,*.o,*.lo,*.la,*.exe,*.swp,*.db,*.bak,*.old,*.dat,*.,tmp,*.mdb,*~,~*"
 
@@ -34,8 +31,6 @@ vim.opt.wildignore = "*.pyc,*.o,*.lo,*.la,*.exe,*.swp,*.db,*.bak,*.old,*.dat,*.,
 vim.opt.wrapscan = false -- 検索をファイルの先頭へループしない
 vim.opt.ignorecase = true -- 検索時の大文字小文字を区別しない
 vim.opt.smartcase = true -- 検索文字列に大文字小文字の両方が含まれる場合、区別する
-vim.opt.incsearch = true -- インクリメンタルサーチを行う
-vim.opt.hlsearch = true
 
 -- Window
 vim.opt.splitbelow = true
@@ -48,14 +43,11 @@ vim.opt.swapfile = false
 
 -- Display
 vim.g.colorterm = os.getenv("COLORTERM")
-if vim.fn.exists("+termguicolors") == 1 then
-  vim.o.termguicolors = true
-end
+vim.o.termguicolors = true
 
 vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
 vim.opt.synmaxcol = 200
 
-vim.cmd([[ syntax enable ]])
 vim.opt.background = "dark"
 
 vim.opt.ambiwidth = "single"
@@ -63,7 +55,6 @@ vim.opt.cursorline = true
 vim.opt.display = "lastline" -- 1 行の文字数が多くても表示
 vim.opt.foldenable = false -- 折りたたみを使用しない
 vim.opt.signcolumn = "yes" -- SignColumn を常に表示
-vim.opt.lazyredraw = false -- 再描画のタイミングを遅らせてスクロール速度を速くする
 vim.opt.showmatch = true -- 括弧を入力した時に対応する括弧へ一瞬ジャンプする
 vim.opt.number = true
 vim.opt.matchtime = 1 -- showmatch の秒数
@@ -71,10 +62,6 @@ vim.opt.list = true -- タブ文字、行末など不可視文字を表示する
 vim.opt.showmode = false -- インサートモードなどの文字を非表示にする
 vim.opt.laststatus = 3 -- 下部のステータスラインを常に表示
 vim.opt.showtabline = 2 -- タブページを常に表示
-
--- Bell
-vim.opt.errorbells = false
-vim.opt.visualbell = false
 
 -- Complete
 vim.opt.complete:append("kspell")
