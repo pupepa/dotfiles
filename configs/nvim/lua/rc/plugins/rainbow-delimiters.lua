73aaf77d7f2a33c4ccb8124520b8516810ed14f8
pupepa <120288786+pupepa@users.noreply.github.com>
2026-08-28 13:15:28 +0900

    chore(nvim): remove rainbow-delimiters config function

--- configs/nvim/lua/rc/plugins/rainbow-delimiters.lua
+++ configs/nvim/lua/rc/plugins/rainbow-delimiters.lua
@@ -5,9 +5,5 @@ return {
     "HiPhish/rainbow-delimiters.nvim",
     event = "BufReadPost",
     submodules = false,
-    config = function()
-      -- patch https://github.com/nvim-treesitter/nvim-treesitter/issues/1124
-      vim.cmd.edit({ bang = true })
-    end,
   },
 }
